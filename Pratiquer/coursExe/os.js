1fb4bbc6f82d52e9da7677971239362ed04ed51d
contant30 <romain.contant@hotmail.fr>
2026-01-19 11:06:17 +0100

    exercice 5 os

---
+++ Pratiquer/coursExe/os.js
@@ -0,0 +1,23 @@
+// Ecrivez la fonction displayOsMessage() qui détecte le systeme d'exploitation
+// Cette fonction affichera un message différend pour Windows, macOS et Linux
+// Appelez la fonction pour la tester
+// Il faudra trouver quel est la chaîne de retour pour chacun des systèmes
+
+const os = require("os");
+
+function displayOsMessage(){
+    
+    const platform = os.platform();
+
+    if(platform === "win32"){
+        console.log("Windows");
+
+    }else if(platform === "darwin"){
+        console.log("macOS");
+
+    }else if(platform === "linux"){
+        console.log("Linux");
+    }
+}
+
+displayOsMessage();
